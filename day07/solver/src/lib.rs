6b8eee76ade08a66da55c6029305c79318cba558
Bryan Burgers <bryan@burgers.io>
2024-12-07 06:59:31 -0600

    07b

--- day07/solver/src/lib.rs
+++ day07/solver/src/lib.rs
@@ -26,7 +26,15 @@ impl solver::Guest for Component {
     }
 
     fn solve_b(input: Vec<solver::CalibrationEquation>) -> u64 {
-        0
+        let mut accum = 0;
+        for equation in input {
+            let equation = CalibrationEquation::from(equation);
+            if equation.can_be_true_second() {
+                accum += equation.test_value;
+            }
+        }
+
+        accum
     }
 }
 
@@ -98,6 +106,82 @@ impl CalibrationEquation {
 
         false
     }
+
+    pub fn can_be_true_second(&self) -> bool {
+        struct StackItem<'a> {
+            current_value: u64,
+            remaining_parameters: &'a [u64],
+        }
+        let mut stack = Vec::new();
+
+        if let Some((left, right)) = self.parameters.split_first() {
+            stack.push(StackItem {
+                current_value: *left,
+                remaining_parameters: right,
+            });
+        }
+
+        while let Some(stack_item) = stack.pop() {
+            let StackItem {
+                current_value,
+                remaining_parameters,
+            } = stack_item;
+
+            if remaining_parameters.is_empty() {
+                if current_value == self.test_value {
+                    return true;
+                }
+                continue;
+            }
+
+            if let Some((next, remaining)) = remaining_parameters.split_first() {
+                let mul_result = current_value * next;
+                let add_result = current_value + next;
+                let con_result = concat(current_value, *next);
+
+                if mul_result == self.test_value && remaining.is_empty() {
+                    return true;
+                }
+                if add_result == self.test_value && remaining.is_empty() {
+                    return true;
+                }
+                if con_result == self.test_value && remaining.is_empty() {
+                    return true;
+                }
+
+                stack.push(StackItem {
+                    current_value: mul_result,
+                    remaining_parameters: remaining,
+                });
+                stack.push(StackItem {
+                    current_value: add_result,
+                    remaining_parameters: remaining,
+                });
+                stack.push(StackItem {
+                    current_value: con_result,
+                    remaining_parameters: remaining,
+                });
+            }
+        }
+
+        false
+    }
+}
+
+fn concat(left: u64, right: u64) -> u64 {
+    if right == 0 {
+        return left * 10;
+    }
+
+    let mut multiplier = 10;
+    loop {
+        if right > multiplier {
+            multiplier *= 10;
+            continue;
+        } else {
+            return left * multiplier + right;
+        }
+    }
 }
 
 #[cfg(test)]
@@ -111,5 +195,14 @@ mod tests {
             parameters: vec![81, 40, 27],
         };
         assert!(equation.can_be_true());
+    }
+
+    #[test]
+    fn test_concat() {
+        assert_eq!(concat(1, 2), 12);
+        assert_eq!(concat(12, 3), 123);
+        assert_eq!(concat(123, 4), 1234);
+        assert_eq!(concat(12, 345), 12345);
+        assert_eq!(concat(12, 0), 120);
     }
 }
